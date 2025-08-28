7b02efaeca7e42e066f981b563780730c2b02cc9
aimsofttech <anshukumar2708@gmail.com>
2025-08-28 15:30:58 +0530

    add link in not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center h-screen text-center">
@@ -6,12 +8,12 @@ export default function NotFound() {
             <p className="mt-4 text-gray-600">
                 Sorry, the page you are looking for does not exist.
             </p>
-            <a
+            <Link
                 href="/"
                 className="mt-6 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
             >
                 Go Home
-            </a>
+            </Link>
         </div>
     );
 }
